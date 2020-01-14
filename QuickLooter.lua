0d9b81aa7523eaa1a966fc16f36590ed5255c391
rainfordays <sebb.tornquist@gmail.com>
2020-01-14 09:23:15 +0100

    Update

--- QuickLooter.lua
+++ QuickLooter.lua
@@ -1,17 +1,17 @@
-local addon = CreateFrame("Frame")
+local A = CreateFrame("Frame")
 
 
-addon:RegisterEvent("LOOT_READY")
+A:RegisterEvent("LOOT_READY")
 
-addon:SetScript("OnEvent", function(self, event, ...)
+A:SetScript("OnEvent", function(self, event, ...)
 	return self[event] and self[event](self, ...)
 end)
 
 
-function addon:LOOT_READY(autoloot)
+function A:LOOT_READY(autoloot)
 	if autoloot then
-		local lootmethod, lootmasterPartyID = GetLootMethod()
-		if lootmethod == "master" and lootmasterPartyID == 0 then return end
+		local method, masterPartyID = GetLootMethod()
+		if method == "master" and masterPartyID == 0 then return end
 		for i = 1, GetNumLootItems() do
 			LootSlot(i)
 		end
